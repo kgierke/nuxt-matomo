b4b7532cc4f536f3b7c9c9a38226e848ec47049b
Kurt Gierke <mail@kurtgierke.com>
2024-07-15 11:55:07 +0200

    chore: add test pages

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    Nuxt module playground!
-  </div>
+  <header>
+    <nav>
+      <NuxtLink to="/"> Home </NuxtLink>
+      <NuxtLink to="/page-1"> Page 1 </NuxtLink>
+      <NuxtLink to="/page-2"> Page 2 </NuxtLink>
+      <NuxtLink to="/page-3"> Page 3 </NuxtLink>
+    </nav>
+  </header>
+  <NuxtPage />
 </template>
 
-<script setup>
-</script>
+<script setup></script>
---
+++ playground/pages/page-3.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Page 3</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
